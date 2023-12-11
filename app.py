e98eb003cb039ae34599395b418ec5cafb1e9731
Nicholas <nick.orsi1992@gmail.com>
2023-12-11 12:18:27 -0800

    Added cupcake model to model file and downloaded the flask debugger tool.

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
 """Flask app for Cupcakes"""
+
+import os
+
+from flask import Flask, request, redirect, render_template, flash
+from flask_debugtoolbar import DebugToolbarExtension
+
+from models import db, connect_db
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'i-have-a-secret'
+
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.debug = True #---> Switch for debug toolbar, False=off True=on
+toolbar = DebugToolbarExtension(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    "DATABASE_URL", "postgresql:///cupcakes")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True #---> When using SQL Alchemy
+
+connect_db(app)
--- models.py
+++ models.py
@@ -1,1 +1,45 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+DEFAULT_IMAGE_URL = "https://tinyurl.com/demo-cupcake"
+
+def connect_db(app):
+    """Connect to the database."""
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
+
+class Cupcake(db.Model):
+    """Cupcake"""
+
+    __tablename__ = 'cupcakes'
+
+    id = db.Column(
+        db.Integer,
+        autoincrement=True,
+        primary_key=True,
+    )
+
+    flavor = db.Column(
+        db.String(50),
+        nullable=False,
+    )
+
+    size = db.Column(
+        db.String(15),
+        nullable=False,
+    )
+
+    rating = db.Column(
+        db.Integer,
+        nullable=False,
+    )
+
+    image_url = db.Column(
+        db.String(500),
+        nullable=False,
+        default=DEFAULT_IMAGE_URL,
+    )
