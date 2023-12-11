df922c7dde0cb80063f7e37a0dda517f03b03a8f
Nicholas <nick.orsi1992@gmail.com>
2023-12-11 14:20:40 -0800

    Added routes to update and delete specific cupcake.

--- app.py
+++ app.py
@@ -30,20 +30,20 @@ def list_cupcakes():
 
     Returns JSON:{cupcakes: [{id, flavor, size, rating, image_url}, ...]}.
     """
-
+    # TODO: Could combine the below into one line
     cupcakes = Cupcake.query.all()
     serialized = [cupcake.serialize() for cupcake in cupcakes]
 
     return jsonify(cupcakes=serialized)
 
 @app.get("/api/cupcakes/<int:cupcake_id>")
-def show_cupcake(cupcake_id):
+def get_cupcake(cupcake_id):
     """
     Returns data about a cupcakes or raise a 404 error if cupcake not found
 
     Returns JSON:{cupcakes: {id, flavor, size, rating, image_url}}.
     """
-
+    # TODO: Could combine the below into one line
     cupcake = Cupcake.query.get_or_404(cupcake_id)
     serialized = cupcake.serialize()
 
@@ -57,19 +57,62 @@ def create_cupcake():
 
     Returns JSON:{cupcakes: {id, flavor, size, rating, image_url}}.
     """
-
+    # TODO: Could make request.jsnn a variable to make it easier to read
     flavor = request.json["flavor"]
     size = request.json["size"]
     rating = request.json["rating"]
     #Whats the best way to import default_image
-    image_url = request.json.get("image_url", DEFAULT_IMAGE_URL)
-
-    new_cupcake = Cupcake(flavor=flavor, size=size, rating=rating,
-                           image_url=image_url)
+    # TODO: Make reque.json['image_url'] or None
+    # image_url = request.json.get("image_url", DEFAULT_IMAGE_URL)
+    image_url = request.json["image_url"] or None
+    # TODO: Combine lines 61 to 69 in one, define the variable below with request.json
+    new_cupcake = Cupcake(
+        flavor=flavor,
+        size=size,
+        rating=rating,
+        image_url=image_url
+    )
 
     db.session.add(new_cupcake)
     db.session.commit()
 
+    # TODO: Could combine the 79 and 82 into one line
     serialized = new_cupcake.serialize()
 
     return (jsonify(cupcake=serialized), 201)
+
+@app.patch("/api/cupcakes/<int:cupcake_id>")
+def update_cupcake(cupcake_id):
+    """
+    Returns updated data about the cupcake or raise a 404 error if cupcake not found
+
+    Returns JSON:{cupcake: {id, flavor, size, rating, image_url}}.
+    """
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+
+
+    cupcake.flavor=request.json["flavor"]
+    cupcake.size=request.json["size"]
+    cupcake.rating=request.json["rating"]
+    cupcake.image_url=request.json["image_url"] or DEFAULT_IMAGE_URL
+
+    db.session.commit()
+
+    return jsonify(cupcake=cupcake.serialize())
+
+@app.delete("/api/cupcakes/<int:cupcake_id>")
+def delete_cupcake(cupcake_id):
+    """
+    Deletes the specific cupcake instance with the ID matching the one in the URL
+    or return 404 error if not found.
+
+    Returns JSON: {deleted: [cupcake-id]}
+    """
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+
+    db.session.delete(cupcake)
+    db.session.commit()
+
+    return jsonify(deleted=cupcake_id)
